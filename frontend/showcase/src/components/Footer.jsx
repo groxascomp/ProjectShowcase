e965ac30b0c0043ea082e833c4172d9faf4483aa
Gian <g.roxas.comp@gmail.com>
2026-09-10 15:54:26 +0800

    Done with Footer

--- frontend/showcase/src/components/Footer.jsx
+++ frontend/showcase/src/components/Footer.jsx
@@ -7,26 +7,26 @@ import { Link } from 'react-router-dom'
 function Footer(){
     return(
         <>
-        <footer class="border-t border-white/10 px-6 md:px-16 py-5  bg-[rgb(10,10,15)]" >
+        <footer class="border-t border-white/10 md:px-16 py-6 pl-10 bg-[rgb(10,10,15)]" >
 
-        <section class="flex justify-between items-center px-8">
+        <section class="flex justify-between items-center px-36">
         <div className='hero-left'>
             <Link to="/home" className="flex items-center gap-3">
                 
                 <div>
-                    <h1 className="text-white font-semibold text-base tracking-wide">Gian Exequiel G. Roxas</h1>
-                    <p className='text-purple-500 font-normal text-sm text-base tracking-wide'> Software Engineer</p>
+                    <h1 className="text-white font-semibold text-sm tracking-wide">Gian Exequiel G. Roxas</h1>
+                    <p className='text-purple-500 font-normal text-xs tracking-wide'> Software Engineer</p>
                 </div>
             </Link>
         </div>
 
-        <nav className="hero-middle flex gap-9.5 " style={{ paddingRight: "20px" }}>
+        <nav className="hero-middle flex gap-9.5 uppercase" style={{ paddingRight: "20px" }}>
             <NavLink 
                 to="/about" 
                 className={({ isActive }) =>
                     isActive
-                    ? "text-sm font-medium transition-colors duration-200 text-white"
-                    : "text-sm font-medium transition-colors duration-200 text-gray-400 hover:text-white"}
+                    ? "text-xs font-medium transition-colors duration-200 text-white"
+                    : "text-xs font-medium transition-colors duration-200 text-gray-400 hover:text-white"}
                 >
                 About
             </NavLink>
@@ -34,8 +34,8 @@ function Footer(){
                 to="/projects" 
                 className={({ isActive }) =>
                     isActive
-                    ? "text-sm font-medium transition-colors duration-200 text-white"
-                    : "text-sm font-medium transition-colors duration-200 text-gray-400 hover:text-white"}
+                    ? "text-xs font-medium transition-colors duration-200 text-white"
+                    : "text-xs font-medium transition-colors duration-200 text-gray-400 hover:text-white"}
                 >
                 Projects
             </NavLink>
@@ -43,8 +43,8 @@ function Footer(){
                 to="/experience" 
                 className={({ isActive }) =>
                     isActive
-                    ? "text-sm font-medium transition-colors duration-200 text-white"
-                    : "text-sm font-medium transition-colors duration-200 text-gray-400 hover:text-white"}
+                    ? "text-xs font-medium transition-colors duration-200 text-white"
+                    : "text-xs font-medium transition-colors duration-200 text-gray-400 hover:text-white"}
                 >
                 Experience
             </NavLink>
@@ -53,8 +53,8 @@ function Footer(){
                 to="/contact" 
                 className={({ isActive }) =>
                     isActive
-                    ? "text-sm font-medium transition-colors duration-200 text-white"
-                    : "text-sm font-medium transition-colors duration-200 text-gray-400 hover:text-white"}
+                    ? "text-xs font-medium transition-colors duration-200 text-white"
+                    : "text-xs font-medium transition-colors duration-200 text-gray-400 hover:text-white"}
                 >
                 Contact
             </NavLink>
@@ -63,19 +63,19 @@ function Footer(){
 
         <div className="hero-right flex gap-3">
             <a href="https://github.com/yourusername">
-                <FontAwesomeIcon icon={faGithub} className="text-xl font-medium transition-colors duration-200 text-gray-400 hover:text-white" />
+                <FontAwesomeIcon icon={faGithub} className="text-lg font-medium transition-colors duration-200 text-gray-400 hover:text-white" />
             </a>
             <a href="https://linkedin.com/in/yourusername">
-                <FontAwesomeIcon icon={faLinkedin} className="text-xl font-medium transition-colors duration-200 text-gray-400 hover:text-white" />
+                <FontAwesomeIcon icon={faLinkedin} className="text-lg font-medium transition-colors duration-200 text-gray-400 hover:text-white" />
             </a>
             <a href="https://facebook.com/yourusername">
-                <FontAwesomeIcon icon={faFacebook} className="text-xl font-medium transition-colors duration-200 text-gray-400 hover:text-white" />
+                <FontAwesomeIcon icon={faFacebook} className="text-lg font-medium transition-colors duration-200 text-gray-400 hover:text-white" />
             </a>
             <a href="https://instagram.com/yourusername">
-                <FontAwesomeIcon icon={faInstagram} className="text-xl font-medium transition-colors duration-200 text-gray-400 hover:text-white" />
+                <FontAwesomeIcon icon={faInstagram} className="text-lg font-medium transition-colors duration-200 text-gray-400 hover:text-white" />
             </a>
             <a href="#info">
-                <FontAwesomeIcon icon={faInfoCircle} className="text-xl font-medium transition-colors duration-200 text-gray-400 hover:text-white" />
+                <FontAwesomeIcon icon={faInfoCircle} className="text-lg font-medium transition-colors duration-200 text-gray-400 hover:text-white" />
             </a>
         </div>
 
@@ -85,7 +85,7 @@ function Footer(){
         </footer>
 
         <section class="px-6 md:px-16 bg-[rgb(10,10,15)]">
-            <div class="border-t border-white/10 w-3/4 mx-auto"></div>
+            <div class="border-t border-white/10 w-8/10 mx-auto"></div>
             <section>
                 <h1 class=" flex justify-center text-xs text-gray-600 py-9 " >© 2026 Gian Exequiel Roxas. Built with React, Spring, and MySQL.</h1>
             </section>
